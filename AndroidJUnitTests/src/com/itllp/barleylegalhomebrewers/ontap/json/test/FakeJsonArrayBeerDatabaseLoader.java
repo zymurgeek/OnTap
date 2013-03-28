62f30ed38ade392a646329e6a26aeadec5917624
David A. Greenbaum <david.alan.greenbaum@gmail.com>
2013-03-28 10:15:56 -0400

    Added JsonArrayBeerDatabaseLoader interface Added JsonUrlBeerDatabaseLoader tests Added FakeJsonArrayBeerDatabaseLoader

---
+++ AndroidJUnitTests/src/com/itllp/barleylegalhomebrewers/ontap/json/test/FakeJsonArrayBeerDatabaseLoader.java
@@ -0,0 +1,27 @@
+package com.itllp.barleylegalhomebrewers.ontap.json.test;
+
+import org.json.JSONArray;
+
+import com.itllp.barleylegalhomebrewers.ontap.JsonArrayBeerDatabaseLoader;
+import com.itllp.barleylegalhomebrewers.ontap.JsonArrayEventDatabaseLoader;
+
+public class FakeJsonArrayBeerDatabaseLoader implements
+		JsonArrayBeerDatabaseLoader {
+	
+	private int loadCount = 0;
+	private JSONArray lastJsonArray;
+	
+	public int getLoadCount() {
+		return loadCount;
+	}
+	
+	@Override
+	public void load(JSONArray jsonArray) {
+		++loadCount;
+		lastJsonArray = jsonArray;
+	}
+
+	public JSONArray getLastLoadArgument() {
+		return lastJsonArray;
+	}
+}
