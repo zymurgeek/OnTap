51da39ee205dc32c60638de39289cd026337dcb0
David A. Greenbaum <zymurgeek@gmail.com>
2014-05-05 15:36:50 -0400

    Add Action Bar to Beer List

--- App/src/com/itllp/barleylegalhomebrewers/ontap/BeerListActivity.java
+++ App/src/com/itllp/barleylegalhomebrewers/ontap/BeerListActivity.java
@@ -3,16 +3,22 @@ package com.itllp.barleylegalhomebrewers.ontap;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class BeerListActivity  extends ActionBarActivity {
 	public static final String SKIP_INSTANTIATION_FOR_TESTING 
 	= "com.itllp.barleylegalhomebrewers.ontap.skipInstantiation_FOR_TESTING";
 	public int eventId = -1;
 	public static final String EVENT_ID = "EVENT_ID";
+
 	
 	public BeerListActivity() {
 	}
 
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,4 +41,34 @@ public class BeerListActivity  extends ActionBarActivity {
         	beerListFrag.setEventId(eventId);
         }
     }
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.beer_list_activity_actions, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_help:
+	            openHelp();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+
+
+	private void openHelp() {
+		CharSequence text = "There is no help yet";
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(this, text, duration);
+		toast.show();	}
 }
